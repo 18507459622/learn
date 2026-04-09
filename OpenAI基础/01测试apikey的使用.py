917596178ba9ca5ab8a076555379cb52e477e881
zxt241612 <2416129810@qq.com>
2026-04-09 17:50:07 +0800

    test

---
+++ OpenAI基础/01测试apikey的使用.py
@@ -0,0 +1,17 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "你是谁能做啥"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
